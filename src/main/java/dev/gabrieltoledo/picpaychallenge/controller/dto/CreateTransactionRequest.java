2486fee9fc8a4ed69e9d3c675f2e2f70c1b0deb0
gctoledo <gabrielcostatoledo@outlook.com>
2025-05-15 16:11:05 -0300

    feat: add validation to create transaction request

--- src/main/java/dev/gabrieltoledo/picpaychallenge/controller/dto/CreateTransactionRequest.java
+++ src/main/java/dev/gabrieltoledo/picpaychallenge/controller/dto/CreateTransactionRequest.java
@@ -4,15 +4,24 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 import dev.gabrieltoledo.picpaychallenge.domain.transaction.Transaction;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class CreateTransactionRequest {
-    BigDecimal  amount;
-    UUID senderId;
-    UUID receiverId;
+
+    @NotNull(message = "Amount is required")
+    @DecimalMin(value = "0.01", inclusive = true, message = "Amount must be greater than or equal to 0.01")
+    private BigDecimal amount;
+
+    @NotNull(message = "Sender ID is required")
+    private UUID senderId;
+
+    @NotNull(message = "Receiver ID is required")
+    private UUID receiverId;
 
     public Transaction toDomain() {
         return new Transaction(amount);
