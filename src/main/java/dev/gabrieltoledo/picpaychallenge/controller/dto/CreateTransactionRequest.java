a1a565bdee056549a5963c9ca88421bb901f9a4b
gctoledo <gabrielcostatoledo@outlook.com>
2025-05-16 11:59:44 -0300

    fix: use setters on build method

--- src/main/java/dev/gabrieltoledo/picpaychallenge/controller/dto/CreateTransactionRequest.java
+++ src/main/java/dev/gabrieltoledo/picpaychallenge/controller/dto/CreateTransactionRequest.java
@@ -15,13 +15,13 @@ public class CreateTransactionRequest {
 
     @NotNull(message = "Amount is required")
     @DecimalMin(value = "0.01", inclusive = true, message = "Amount must be greater than or equal to 0.01")
-    private BigDecimal amount;
+    private final BigDecimal amount;
 
     @NotNull(message = "Sender ID is required")
-    private UUID senderId;
+    private final UUID senderId;
 
     @NotNull(message = "Receiver ID is required")
-    private UUID receiverId;
+    private final UUID receiverId;
 
     public Transaction toDomain() {
         return new Transaction(amount);
--- src/main/java/dev/gabrieltoledo/picpaychallenge/controller/dto/UserResponse.java
+++ src/main/java/dev/gabrieltoledo/picpaychallenge/controller/dto/UserResponse.java
@@ -7,8 +7,10 @@ import java.util.UUID;
 import dev.gabrieltoledo.picpaychallenge.domain.user.User;
 import dev.gabrieltoledo.picpaychallenge.domain.user.UserType;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class UserResponse {
     private UUID id;
     private String firstName;
@@ -23,14 +25,14 @@ public class UserResponse {
     public static UserResponse build(User user) {
         UserResponse userResponse = new UserResponse();
 
-        userResponse.id = user.getId();
-        userResponse.firstName = user.getFirstName();
-        userResponse.lastName = user.getLastName();
-        userResponse.document = user.getDocument();
-        userResponse.email = user.getEmail();
-        userResponse.userType = user.getUserType();
-        userResponse.balance = user.getBalance();
-        userResponse.createdAt = user.getCreatedAt();
+        userResponse.setId(user.getId());
+        userResponse.setFirstName(user.getFirstName());
+        userResponse.setLastName(user.getLastName());
+        userResponse.setDocument(user.getDocument());
+        userResponse.setEmail(user.getEmail());
+        userResponse.setUserType(user.getUserType());
+        userResponse.setBalance(user.getBalance());
+        userResponse.setCreatedAt(user.getCreatedAt());
 
         return userResponse;
     }
